c2b998afc8da54171bab4834df6bd096395b6cf8
Skyler James <15920401+skyjay1@users.noreply.github.com>
2019-06-11 15:32:29 -0600

    Update to latest
    Update to reccommended version of Forge. Update to latest 1.12 versions of Extra Golems, TConstruct, and Mantle.

--- src/main/java/com/golems_tcon/init/TconGolems.java
+++ src/main/java/com/golems_tcon/init/TconGolems.java
@@ -15,8 +15,8 @@ public class TconGolems {
 	
 	public static final String MODID = "golems_tcon";
 	public static final String NAME = "Tinkers' Construct Golems";
-	public static final String VERSION = "7.1.1-1";
-	public static final String DEPS = "required-after:golems@[7.1.1,);required-after:tconstruct@[1.12.2-2.10.0.81,)";
+	public static final String VERSION = "7.1.5-0";
+	public static final String DEPS = "required-after:golems@[7.1.5,);required-after:tconstruct@[1.12.2-2.12.0.135,)";
 	
 	@SidedProxy(clientSide = "com." + MODID + ".proxy.ClientProxy", serverSide = "com." + MODID + ".proxy.CommonProxy")
 	public static CommonProxy proxy;
